9062f84c176458f306a29c818da764a63fbc9a5b
Tsiory Robsona <robsonatsiory07@gmail.com>
2024-02-29 14:23:43 +0300

    [fix]

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ mongoose.Promise = global.Promise;
 // remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud s
 //const uri = 'mongodb+srv://mb:P7zM3VePm0caWA1L@cluster0.zqtee.mongodb.net/assignments?retryWrites=true&w=majority';
 const uri = 'mongodb+srv://mbds:Admin123@cluster0.vruseb4.mongodb.net/assignments?retryWrites=true&w=majority';
+
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify:false
+  useFindAndModify: false
 };
 
 mongoose.connect(uri, options)
@@ -21,7 +22,7 @@ mongoose.connect(uri, options)
     console.log("Connecté à la base MongoDB assignments dans le cloud !");
     console.log("at URI = " + uri);
     console.log("vérifiez with http://localhost:8010/api/assignments que cela fonctionne")
-    },
+  },
     err => {
       console.log('Erreur de connexion: ', err);
     });
@@ -35,7 +36,7 @@ app.use(function (req, res, next) {
 });
 
 // Pour les formulaires
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 let port = process.env.PORT || 8010;
@@ -43,21 +44,23 @@ let port = process.env.PORT || 8010;
 // les routes
 const prefix = '/api';
 
+// http://serveur..../assignments
+app.route(prefix + '/assignments')
+  .post(assignment.postAssignment)
+  .put(assignment.updateAssignment)
+  .get(assignment.getAssignments);
 
 app.route(prefix + '/assignments/:id')
   .get(assignment.getAssignment)
   .delete(assignment.deleteAssignment);
 
 
-app.route(prefix + '/assignments')
-  .get(assignment.getAssignments)
-  .post(assignment.postAssignment)
-  .put(assignment.updateAssignment);
-
 // On démarre le serveur
 app.listen(port, "0.0.0.0");
 console.log('Serveur démarré sur http://localhost:' + port);
 
 module.exports = app;
+
+
 
 
